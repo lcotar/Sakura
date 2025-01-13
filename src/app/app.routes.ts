b61628ce7ada610bec12a5fa06548db411a85c37
Lea Cotar <lea.cotar@gmx.ch>
2025-01-13 18:49:24 +0100

    updated Files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainContentComponent },
+  { path: 'imprint', component: ImprintComponent },
+];
